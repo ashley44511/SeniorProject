fe903a732750255f0a3c5f385525065dec69ef51
Train050 <keegan.sepiol@ufl.edu>
2025-02-14 00:08:13 -0500

    Added consumable, weapon, material, and trap item types

--- Assets/Hotbar/Item.cs
+++ Assets/Hotbar/Item.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 
+public enum ItemType
+{
+    Consumable,
+    Material,
+    Weapon,
+    Trap
+}
+
 public class Item : MonoBehaviour
 {
     public int ID;
     public string itemName;
-
+    public ItemType itemType;
 }
