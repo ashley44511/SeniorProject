0c551457f36ae84be079acb9706e3a55ef16a2d7
Train050 <keegan.sepiol@ufl.edu>
2025-03-29 00:59:50 -0400

    Fixed Scene Transition Bug

--- Assets/Scripts/Misc Scripts/SceneTransitionWithE.cs
+++ Assets/Scripts/Misc Scripts/SceneTransitionWithE.cs
@@ -2,43 +2,45 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneTransitionWithE : MonoBehaviour
 {
-    public string sceneName;
-    public GameObject ePrompt;
+    public string sceneName; // String name to the game scene to load
+    public GameObject ePrompt; // Interact image
+    private bool playerInTrigger = false; // Tracks if the player is standing in the trigger volume
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ePrompt.gameObject.SetActive(false);
     }
 
+    //Running for every frame in the game (OnTriggerStay was skipping frames so I had to switch it)
+    void Update()
+    {
+        //If the user is hitting E and is within the trigger, then the next scene will play
+        if (Input.GetKeyDown(KeyCode.E) && playerInTrigger == true)
+        {
+            playerInTrigger = false;
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collision detected");
+        //If the player enters the trigger, the prompt appears and the boolean is updated
         if (collision.gameObject.tag == "Player")
         {
+            playerInTrigger = true;
             //Will change this to space bar later, when we have a sprite for it
             //This will probably be a canvas overlay or just a floating text saying press space to return
             ePrompt.gameObject.SetActive(true);
-            Debug.Log("Player in area to return to parking lot");
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            //Will change this to space bar later, when we have a sprite for it
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                SceneManager.LoadScene(sceneName);
-            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //If the player leaves the trigger, the prompt disappears and the boolean is updated accordingly
         if (collision.gameObject.tag == "Player")
         {
+            playerInTrigger = false;
             ePrompt.gameObject.SetActive(false);
-            Debug.Log("Player left the return to parking lot area");
         }
     }
 }
