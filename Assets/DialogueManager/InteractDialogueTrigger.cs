9b1a36b83739f5cd6f2d700ba5f5a0ae8d5dd98f
Train050 <keegan.sepiol@ufl.edu>
2025-04-16 01:54:46 -0400

    Pamphlet and disable E after item used in Dialogue Interact

--- Assets/DialogueManager/InteractDialogueTrigger.cs
+++ Assets/DialogueManager/InteractDialogueTrigger.cs
@@ -128,7 +128,7 @@ public class InteractDialogueTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !hasBeenUsed)
         {
             InteractImage.gameObject.SetActive(true);
         }
