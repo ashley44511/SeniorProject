3e6ddacc24c395b1e1a99f9e2961d33f9f0ad5a8
Train050 <keegan.sepiol@ufl.edu>
2025-04-18 18:45:04 -0400

    Fixed camera glitch from pause menu main menu button

--- Assets/Pause Menu/LoadScene.cs
+++ Assets/Pause Menu/LoadScene.cs
@@ -5,6 +5,8 @@ public class LoadScene : MonoBehaviour
 {
     public void LoadSceneByName(string sceneName)
     {
+        //Resuming time on the stage (fixes pause issue)
+        Time.timeScale = 1;
         SceneManager.LoadScene(sceneName);
     }
 
